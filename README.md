6ed4ac3fe99961070e9ba8c54b1602202245fd7f
David Mostoller <dmostoller@gmail.com>
2024-06-09 22:43:37 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Upload Camper Photo
+
+
+The app will allow a user to upload a photo and apply a photo transormation through Cloudinary, then view the photo.
+
---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>Upload Camper Photo</h1>
+      <div className="card">
+        <button>
+            Upload Photo
+        </button>
+        <p>
+          Select Template then Upload camper photo with the button above
+        </p>
+      </div>
+    </>
+  )
+}
+
+export default App
