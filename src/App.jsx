949f2823e3263ada7427d9cce32dccc5142f87cb
David Mostoller <dmostoller@gmail.com>
2024-06-09 23:57:36 -0400

    basic functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,26 @@
-import { useState } from 'react'
+import React, { useState } from 'react'
 import './App.css'
+import UploadWidget from "./UploadWidget";
+
+
+
 
 function App() {
-
+    const [error, setError] = useState(null);
+    const [imageUrl, setImageUrl] = useState();
   return (
     <>
       <h1>Upload Camper Photo</h1>
       <div className="card">
-        <button>
-            Upload Photo
-        </button>
+        {/* <button className='ui circular icon massive button'>
+            <i className='camera icon'></i>
+        </button> */}
+        <UploadWidget onSetImageUrl={setImageUrl}/>
+        {imageUrl && 
+            <img className="ui centered image large" src={imageUrl} alt=""></img>
+        }
+        {error && 
+        <p>{error}</p>}
         <p>
           Select Template then Upload camper photo with the button above
         </p>
---
+++ src/UploadWidget.jsx
@@ -0,0 +1,34 @@
+import React, { useEffect, useRef } from "react";
+
+function UploadWidget({onSetImageUrl}) {
+
+    const cloudinaryRef = useRef();
+    const widgetRef = useRef();
+
+    useEffect(() => {
+        cloudinaryRef.current = window.cloudinary;
+        widgetRef.current = cloudinaryRef.current.createUploadWidget({
+            cloudName: 'ddp2xfpyb',
+            uploadPreset: 'campercard-demo',
+            multiple: false,  //restrict upload to a single file
+            // sources: [ "local", "url"], // restrict the upload sources to URL and local files
+        }, function(error, result) { 
+            if (error) {
+                console.log(error)
+            }
+            if (!error && result && result.event === "success") {
+                console.log(result.info);
+                onSetImageUrl(result.info.secure_url);
+    }});
+    }, [onSetImageUrl])
+
+return (
+    <>
+    <button type="button" className="ui circular icon blue massive button " onClick={() => widgetRef.current.open()}>
+        <i className='camera icon'></i>
+    </button>
+    </>
+)
+
+}
+export default UploadWidget;
